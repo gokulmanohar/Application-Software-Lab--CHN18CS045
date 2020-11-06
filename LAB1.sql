8e3c68f61214aae8b10733b1144b70cdfaa963e0
Gokul Manohar <gokulmanohar7@gmail.com>
2020-11-06 15:10:46 +0530

    First LAB SQL Queries.

---
+++ LAB1.sql
@@ -0,0 +1,30 @@
+USE S5D_26;
+
+CREATE TABLE Student(
+    sno INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    marks DECIMAL(8, 2) NOT NULL,
+    department VARCHAR(20) NOT NULL,
+    PRIMARY KEY (sno)
+);
+
+ALTER TABLE Student
+ADD COLUMN age SMALLINT NOT NULL;
+
+ALTER TABLE Student
+MODIFY department VARCHAR(40);
+
+ALTER TABLE Student
+DROP COLUMN marks;
+
+ALTER TABLE Student
+RENAME TO Students;
+
+describe Students;
+
+INSERT INTO Students
+VALUES(1, "JOHN", "Computer Science", 21);
+
+TRUNCATE TABLE Students;
+
+DROP TABLE Students;
